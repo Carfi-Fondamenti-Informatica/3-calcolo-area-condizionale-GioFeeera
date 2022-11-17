2170734f8e792f7f6bed5c6168fb0a30e0195fa9
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-11-17 22:23:03 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
-#include <iostream>
-using namespace std;
-
 int main() {
     float a=0, b=0, A=0;
     int opzione=0;
-   cin >> a >> b;
+    cout << "Inserire le misure dei lati a: " << endl;
+    cin >> a;
+    cout << "E b: " << endl;
+    cin >> b;
+    cout << "Scegliere l'area da calcolare: \n -Digitare 0 per Area triangolo; \n -Digitare 1 per area quadrato; \n -Digitare 2 per area rettangolo" << endl;
     cin >> opzione;
     switch (opzione) {
         case 0:
             A= (a*b)/2;
-            cout << A << endl;
+            cout << "Il risultato dell'area richiesta e\':  " << A << endl;
             break;
         case 1:
             A= a*a;
-           cout << A << endl;
+            cout << "Il risultato dell'area richiesta e\':  " << A << endl;
             break;
         case 2:
             A= a*b;
-            cout << A << endl;
+            cout << "Il risultato dell'area richiesta e\':  " << A << endl;
             break;
         default :
             cout << "opzione non valida" << endl;
